2ad302e88a5af08fad2523897208116188fa1a78
chlendyd7 <chlendyd7@naver.com>
2023-06-11 23:20:44 +0900

    06_11

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     #Third Apps
     'bootstrap4',
     'debug_toolbar',
+    'django_pydenticon',
 
     #Local Apps
     'accounts',
@@ -140,3 +141,11 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Email with Send Grid
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- askcompany/urls.py
+++ askcompany/urls.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,RedirectView
 from django.contrib.auth.decorators import login_required
-
+from django_pydenticon.views import image as pydenticon_image
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
+    path('identicon/image/<path:data>/', pydenticon_image, name='pydenticon_image'),
+    # path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
+    path('', RedirectView.as_view(pattern_name='instagram:index'), name='root'),
     path('accounts/', include('accounts.urls')),
 ]
 
